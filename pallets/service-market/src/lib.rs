9cd0dec0c27b9476982404a3ffcc097d9b1c8932
Alex Chen <alex.chen31337@gmail.com>
2026-03-05 10:13:50 +1100

    fix: remove unused Saturating import in pallet-service-market

--- pallets/service-market/src/lib.rs
+++ pallets/service-market/src/lib.rs
@@ -51,7 +51,6 @@ pub mod pallet {
     use frame_system::pallet_prelude::*;
     use pallet_reputation::ReputationManager;
     use sp_runtime::traits::AccountIdConversion;
-    use sp_runtime::traits::Saturating;
 
     // =========================================================
     // Type Aliases
